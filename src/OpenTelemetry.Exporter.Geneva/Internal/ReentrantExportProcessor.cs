0855636663d7c74b80b726d67ee90096e0be47bc
Mikel Blanchard <mblanchard@macrosssoftware.com>
2024-10-24 12:49:13 -0700

    Remove lambda compile from Geneva ReentrantExportProcessor.

--- src/OpenTelemetry.Exporter.Geneva/Internal/ReentrantExportProcessor.cs
+++ src/OpenTelemetry.Exporter.Geneva/Internal/ReentrantExportProcessor.cs
@@ -1,9 +1,6 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-using System.Linq.Expressions;
-using System.Reflection;
-
 namespace OpenTelemetry.Exporter.Geneva;
 
 // This export processor exports without synchronization.
@@ -14,7 +11,8 @@ namespace OpenTelemetry.Exporter.Geneva;
 internal class ReentrantExportProcessor<T> : BaseExportProcessor<T>
     where T : class
 {
-    private static readonly Func<T, Batch<T>> CreateBatch = BuildCreateBatchDelegate();
+    [ThreadStatic]
+    private static T[]? threadStorage;
 
     public ReentrantExportProcessor(BaseExporter<T> exporter)
         : base(exporter)
@@ -23,16 +21,10 @@ internal class ReentrantExportProcessor<T> : BaseExportProcessor<T>
 
     protected override void OnExport(T data)
     {
-        this.exporter.Export(CreateBatch(data));
-    }
+        var storage = threadStorage ??= new T[1];
 
-    private static Func<T, Batch<T>> BuildCreateBatchDelegate()
-    {
-        var flags = BindingFlags.Instance | BindingFlags.NonPublic;
-        var ctor = typeof(Batch<T>).GetConstructor(flags, null, new Type[] { typeof(T) }, null)
-            ?? throw new InvalidOperationException("Batch ctor accepting a single item could not be found reflectively");
-        var value = Expression.Parameter(typeof(T), null);
-        var lambda = Expression.Lambda<Func<T, Batch<T>>>(Expression.New(ctor, value), value);
-        return lambda.Compile();
+        storage[0] = data;
+
+        this.exporter.Export(new Batch<T>(storage, 1));
     }
 }
